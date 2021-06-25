1ff5387fab32f0fdf6a2a9c6d8f4f99ce032809f
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-24 21:24:46 -0400

    Clean code in transporter.js to get rid of pseudocode

--- utils/transporter.js
+++ utils/transporter.js
@@ -13,26 +13,19 @@ const transporter = nodemailer.createTransport({
 
 const styling = 'styling';
 
-// Test script (from https://nodemailer.com/about/):
+// Adapted test script
 async function main() {
-
-    // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: '"Sample Email" <sample_email@example.com>', // sender address
-        to: '"Weirdo Email" <target_email@example.com>', // list of receivers
-        subject: "Test 10", // Subject line
-        text: "You've got this!", // plain text body
+        from: '"Sample Email" <sample_email@example.com>',
+        to: '"Weirdo Email" <target_email@example.com>',
+        subject: "Test 10",
+        text: "You've got this!",
         html: `<h1>This is an email</h1>
         <b>This will be where the certificate is located</b>,
-        <p style="color:violet;">and we'll also have some ${styling} here.</p>` // html body
+        <p style="color:violet;">and we'll also have some ${styling} here.</p>`
     });
 
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 };
 
 main().catch(console.error);
