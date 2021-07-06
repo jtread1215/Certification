9bc51bc11608e4afe0494ce0a5b54a22d620cdb5
Roy Sizemore <rsizemore@swampcastle.home>
2021-07-06 17:27:58 -0400

    Add credentials security to transporter; update .env and .gitignore

--- utils/transporter.js
+++ utils/transporter.js
@@ -1,18 +1,9 @@
-/* Libre Baskerville font (for the certificate):
-<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&display=swap" rel="stylesheet"></link> */
-
 // Trim unnecessary whitespace from email address field
 'use strict';
 
 const nodemailer = require('nodemailer');
-const mongoose = require('mongoose');
 
 const email = async () => {
-    let eventName = mongoose.regitstration.event; // Pseudocode: not sure how we'll implement this, yet
-    let fName = mongoose.regitstration.attendee.first_name; // Pseudocode: not sure how we'll implement this, yet
-    let lName = mongoose.regitstration.attendee.last_name; // Pseudocode: not sure how we'll implement this, yet
-    let commaTitle = ', ' + mongoose.regitstration.attendee.title; // Pseudocode: not sure how we'll implement this, yet
-
     // Attach cert file to email
     let message = {
         attachments: [
@@ -28,8 +19,8 @@ const email = async () => {
         host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-            user: 'lauren96@ethereal.email',
-            pass: 'hSdfHX39XPheuWwbcf'
+            user: process.env.NODEMAILER_ADDRESS,
+            pass: process.env.NODEMAILER_PASSWORD
         }
     });
 
@@ -40,15 +31,13 @@ const email = async () => {
         subject: "Test 10",
         text: 'To view your certificate, please enable HTML emails.',
         html: `
-            <div class='cert-bg container'>
-                <div class='card'>
-                    <h1 class='event'>${eventName}</h1>
-                    <p>${fName} ${lName}${commaTitle}</p>
+            <div>
+                <div>
+                    <h1>Title</h1>
+                    <p style="color: purple">Some text</p>
                 </div>
             </div>`
     });
-
-    // console.log('Message sent: %s', emailInfo.messageId);
 };
 
 email().catch(console.error);
